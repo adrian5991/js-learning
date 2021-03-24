09171e91d968746a702c2726154c7ce4fca276e5
Adrian <angquiangco7@gmail.com>
2021-03-23 21:13:16 -0400

    added food object

--- snake-game/snake.js
+++ snake-game/snake.js
@@ -11,6 +11,7 @@ let down = false;
 document.addEventListener("keydown", (e) => {
   if (e.key == "Right" || e.key == "ArrowRight") {
     right = true;
+    mySnake.move(right);
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
     left = true;
   } else if (e.key == "Up" || e.key == "ArrowUp") {
@@ -32,22 +33,45 @@ document.addEventListener("keyup", (e) => {
   }
 });
 
+function Food(height, width) {
+  let food = Object.create(Food.prototype);
+  food.xPos = getRandomIntInclusive(500, canvas.width);
+  food.yPos = getRandomIntInclusive(500, canvas.height);
+  food.height = height;
+  food.width = width;
+  return food;
+}
+
+Food.prototype.drawFood = function () {
+  ctx.beginPath();
+  ctx.rect(this.xPos, this.yPos, this.width, this.height);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+};
+
+Food.prototype.draw = function () {
+  this.drawFood();
+};
+
 class Snake {
-  constructor(xPos, yPos, height, width) {
+  constructor(xPos, yPos, height, width, radius) {
     this.xPos = xPos;
     this.yPos = yPos;
     this.height = height;
     this.width = width;
+    this.radius = radius;
   }
+
   drawSnake() {
     ctx.beginPath();
-    ctx.rect(this.xPos, this.yPos, 50, 50);
-    ctx.fillStyle = "#FF0000";
+    ctx.arc(this.xPos, this.yPos, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
   }
-  draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  move() {
     this.drawSnake();
     if (right) {
       this.xPos += 10;
@@ -70,71 +94,36 @@ class Snake {
         this.yPos = canvas.height - this.height;
       }
     }
-    requestAnimationFrame(() => this.draw());
   }
 }
 
-const mySnake = new Snake(canvas.width / 2, canvas.height / 2, 50, 50, ctx, canvas);
-requestAnimationFrame(() => mySnake.draw()); // the first parameter must call draw() otherwise this.xPos will be undefined
+const mySnake = new Snake(canvas.width / 2, canvas.height / 2, 50, 50, 20);
+const myFood = Food(50, 50);
+let score = 0;
 
-/* No class */
-// function Snake(xPos, yPos, height, width) {
-//   this.xPos = xPos;
-//   this.yPos = yPos;
-//   this.height = height;
-//   this.width = width;
-// }
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  mySnake.move();
+  myFood.draw();
+  if (
+    mySnake.xPos > myFood.xPos &&
+    mySnake.xPos < myFood.xPos + myFood.width &&
+    mySnake.yPos > myFood.yPos &&
+    mySnake.yPos < myFood.yPos + myFood.height
+  ) {
+    myFood.xPos = getRandomIntInclusive(500, canvas.width - 500);
+    myFood.yPos = getRandomIntInclusive(500, canvas.height - 500);
+    score += 10;
+    console.clear();
+    console.log(score);
+  }
+  requestAnimationFrame(animate);
+}
 
-// Snake.prototype.drawSnake = function () {
-//   ctx.beginPath();
-//   ctx.rect(this.xPos, this.yPos, 50, 50);
-//   ctx.fillStyle = "#FF0000";
-//   ctx.fill();
-//   ctx.closePath();
-// };
+requestAnimationFrame(animate);
 
-// Snake.prototype.draw = function () {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   this.drawSnake();
-//   if (right) {
-//     this.xPos += 10;
-//     if (this.xPos + this.width > canvas.width) {
-//       this.xPos = canvas.width - this.width;
-//     }
-//   } else if (left) {
-//     this.xPos -= 10;
-//     if (this.xPos < 0) {
-//       this.xPos = 0;
-//     }
-//   } else if (up) {
-//     this.yPos -= 10;
-//     if (this.yPos < 0) {
-//       this.yPos = 0;
-//     }
-//   } else if (down) {
-//     this.yPos += 10;
-//     if (this.yPos + this.height > canvas.height) {
-//       this.yPos = canvas.height - this.height;
-//     }
-//   }
-//   requestAnimationFrame(() => this.draw());
-// };
-
-/* No prototype version */
-// const snakeProps = {
-//   moveRight: function (units) {},
-//   moveLeft: function (units) {},
-//   moveUp: function (units) {},
-//   moveDown: function (units) {},
-// };
-// function SnakeThree(xPos, yPos) {
-//   // let snake = {};
-//    // let snake = Object.create(Snake.prototype);
-// snake.xPos = xPos;
-// snake.yPos = yPos;
-// snake.height = height;
-// snake.width = width;
-// return snake;
-/* above is equivalent to bottom + `new` keyword*/
-// }
-// let z = SnakeTwo(1, 1);
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
