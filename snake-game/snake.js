9a3b88f67b32d981f393f68a9a5498d73c715589
Adrian <angquiangco7@gmail.com>
2021-03-24 22:28:45 -0400

    create snake body

--- snake-game/snake.js
+++ snake-game/snake.js
@@ -7,20 +7,26 @@ let right = false;
 let left = false;
 let up = false;
 let down = false;
+let bodyCount = 0;
+let bodyCountArray = [];
+let snakes = [];
+let facingDir = "east";
 
 document.addEventListener("keydown", (e) => {
   if (e.key == "Right" || e.key == "ArrowRight") {
     right = true;
-    mySnake.move(right);
+    facingDir = "east";
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
     left = true;
+    facingDir = "west";
   } else if (e.key == "Up" || e.key == "ArrowUp") {
     up = true;
+    facingDir = "north";
   } else if (e.key == "Down" || e.key == "ArrowDown") {
     down = true;
+    facingDir = "south";
   }
 });
-
 document.addEventListener("keyup", (e) => {
   if (e.key == "Right" || e.key == "ArrowRight") {
     right = false;
@@ -55,51 +61,64 @@ Food.prototype.draw = function () {
 };
 
 class Snake {
-  constructor(xPos, yPos, height, width, radius) {
+  constructor(
+    xPos = canvas.width / 2,
+    yPos = canvas.height / 2,
+    height = 0,
+    width = 0,
+    radius = 20,
+    color = "blue"
+  ) {
     this.xPos = xPos;
     this.yPos = yPos;
     this.height = height;
     this.width = width;
     this.radius = radius;
+    this.color = color;
   }
 
-  drawSnake() {
+  drawSnake(xPos, yPos, color) {
     ctx.beginPath();
-    ctx.arc(this.xPos, this.yPos, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(xPos, yPos, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = color;
     ctx.fill();
     ctx.closePath();
   }
 
   move() {
-    this.drawSnake();
+    this.drawSnake(this.xPos, this.yPos);
     if (right) {
       this.xPos += 10;
       if (this.xPos + this.width > canvas.width) {
-        this.xPos = canvas.width - this.width;
+        this.xPos = 0;
       }
     } else if (left) {
       this.xPos -= 10;
       if (this.xPos < 0) {
-        this.xPos = 0;
+        this.xPos = canvas.width - this.width;
       }
     } else if (up) {
       this.yPos -= 10;
       if (this.yPos < 0) {
-        this.yPos = 0;
+        this.yPos = canvas.height - this.height;
       }
     } else if (down) {
       this.yPos += 10;
       if (this.yPos + this.height > canvas.height) {
-        this.yPos = canvas.height - this.height;
+        this.yPos = 0;
       }
     }
   }
 }
 
-const mySnake = new Snake(canvas.width / 2, canvas.height / 2, 50, 50, 20);
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+const mySnake = new Snake();
 const myFood = Food(50, 50);
-let score = 0;
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -113,17 +132,48 @@ function animate() {
   ) {
     myFood.xPos = getRandomIntInclusive(500, canvas.width - 500);
     myFood.yPos = getRandomIntInclusive(500, canvas.height - 500);
-    score += 10;
-    console.clear();
-    console.log(score);
+    bodyCount += 1;
+    const newSnake = new Snake();
+    bodyCountArray.push(bodyCount);
+    snakes.push(newSnake);
+    console.log(snakes, mySnake);
   }
+  // console.log(facingDir);
+  if (facingDir === "east") {
+    for (let i = 0; i < bodyCount; i++) {
+      snakes[i].drawSnake(
+        mySnake.xPos - mySnake.radius * 2 * bodyCountArray[i],
+        mySnake.yPos,
+        "red"
+      );
+    }
+  } else if (facingDir === "west") {
+    for (let i = 0; i < bodyCount; i++) {
+      snakes[i].drawSnake(
+        mySnake.xPos + mySnake.radius * 2 * bodyCountArray[i],
+        mySnake.yPos,
+        "red"
+      );
+    }
+  } else if (facingDir === "north") {
+    for (let i = 0; i < bodyCount; i++) {
+      snakes[i].drawSnake(
+        mySnake.xPos,
+        mySnake.yPos - mySnake.radius * 2 * bodyCountArray[i],
+        "red"
+      );
+    }
+  } else if (facingDir === "south") {
+    for (let i = 0; i < bodyCount; i++) {
+      snakes[i].drawSnake(
+        mySnake.xPos,
+        mySnake.yPos + mySnake.radius * 2 * bodyCountArray[i],
+        "red"
+      );
+    }
+  }
+
   requestAnimationFrame(animate);
 }
 
 requestAnimationFrame(animate);
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}
