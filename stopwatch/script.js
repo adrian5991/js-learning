95c4c464946f7be2781443278b3196294094d711
Adrian <angquiangco7@gmail.com>
2021-04-02 14:04:48 -0400

    stopwatch

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -1,26 +1,21 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+/*
+  - more js practice
+  - using browser date object is more accurate than incrementing via any js function
+  - beginner flexbox
+*/
+
 const timer = document.querySelector("#timer");
-const stopButton = document.querySelector("#stop");
-const startButton = document.querySelector("#start");
+const startStopButton = document.querySelector("#startStop");
 const resetButton = document.querySelector("#reset");
 timer.innerHTML = `00:00:00`;
-
+let intervalId = null;
 const stopWatch = {
   seconds: 0,
   minutes: 0,
   hours: 0,
 };
 
-stopButton.addEventListener("click", () => {
-  clearInterval(intervalId);
-});
-
-startButton.addEventListener("click", () => {
-  const start = Date.now();
-  const offset = stopWatch.seconds;
-  intervalId = setInterval(() => updateTimer(start, offset), 1000);
-});
+startStopButton.addEventListener("click", start);
 
 resetButton.addEventListener("click", () => {
   clearInterval(intervalId);
@@ -28,7 +23,27 @@ resetButton.addEventListener("click", () => {
   stopWatch.seconds = 0;
   stopWatch.minutes = 0;
   stopWatch.hours = 0;
+  startStopButton.classList.remove("active-button");
+  startStopButton.innerHTML = "Start";
 });
+
+function start() {
+  const now = Date.now();
+  const offset = stopWatch.seconds;
+  intervalId = setInterval(() => updateTimer(now, offset), 1000);
+  startStopButton.removeEventListener("click", start);
+  startStopButton.addEventListener("click", stop);
+  startStopButton.innerHTML = "Stop";
+  startStopButton.classList.add("active-button");
+}
+
+function stop() {
+  clearInterval(intervalId);
+  startStopButton.removeEventListener("click", stop);
+  startStopButton.addEventListener("click", start);
+  startStopButton.innerHTML = "Start";
+  startStopButton.classList.remove("active-button");
+}
 
 function updateTimer(start, offset) {
   let delta = Date.now() - start;
